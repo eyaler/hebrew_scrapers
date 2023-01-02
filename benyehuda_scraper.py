68fe104eb0688f4588b7857633135213e50062c4
Eyal Gruss <eyalgruss@gmail.com>
2023-01-02 16:32:02 +0200

    Update benyehuda_scraper.py

--- benyehuda_scraper.py
+++ benyehuda_scraper.py
@@ -1,9 +1,11 @@
 from collections import Counter, defaultdict
+from datetime import datetime
 import os
 import re
-import requests
 import sys
 import time
+
+import requests
 
 
 api_url = 'https://benyehuda.org/api/v1/search'
@@ -16,10 +18,10 @@ strip_footer = True
 timeout_secs = 60
 sleep_secs = 10
 retries = 3
-base_folder = 'd:/data/text/heb/benyehuda'
+base_folder = 'c:/data/text/heb/benyehuda'
 
 
-if len(sys.argv) > 1:
+if sys.argv[1:]:
     key = sys.argv[1]
 elif not key:
     print('Usage: python benyehuda_scraper.py API_KEY')
@@ -27,12 +29,13 @@ elif not key:
     sys.exit(1)
 
 all_periods = ['modern', 'revival', 'enlightenment', 'medieval', 'ancient', 'no_period']
-periods = periods or all_periods
+periods = sorted(periods, key=lambda p: all_periods.index(p)) or all_periods
 bad_urls = []
-all_files = defaultdict(list)
 total_dl = 0
 total_files = 0
 bad_periods = Counter()
+all_periods_count = 0
+all_before_folder_files_count = 0
 with open('benyehuda_extra_files.txt', 'w') as fextra:
     for period in periods:
         folder = os.path.join(base_folder, file_format, period)
@@ -41,7 +44,9 @@ with open('benyehuda_extra_files.txt', 'w') as fextra:
         page = 1
         total_count = None
         files = []
+        cnt_new = 0
         cnt_dl = 0
+        before_folder_files_count = len(os.listdir(folder))
         while not total_count or i < total_count:
             try:
                 request_args = dict(file_format=file_format, page=page, periods=[period])
@@ -56,13 +61,14 @@ with open('benyehuda_extra_files.txt', 'w') as fextra:
             json = response.json()
             if total_count is None:
                 total_count = json['total_count']
-                print(f'Fetching period={period} total_count={total_count}')
+                effective_total_count = total_count - all_periods_count*(period == 'no_period')
+                folder_files = os.listdir(folder)
+                print(f"Fetching period={period} total_count={effective_total_count} to_download={max(0, effective_total_count - len(folder_files))}")
+                files = []
             elif total_count != json['total_count']:
+                total_count = None
                 i = 0
                 page = 1
-                total_count = None
-                files = []
-                cnt_dl = 0
                 continue
             for item in json['data']:
                 i += 1
@@ -78,18 +84,18 @@ with open('benyehuda_extra_files.txt', 'w') as fextra:
                 path = os.path.join(folder, file)
                 if skip_exist and os.path.exists(path) and os.path.getsize(path):
                     continue
-                print(f'{i}/{total_count}:', download_url)
+                cnt_new += 1
+                print(f'({total_dl + 1}) new={cnt_new}/{total_count - (all_before_folder_files_count if period == "no_period" else before_folder_files_count)} all={i}/{total_count}:', download_url)
                 response = None
                 for j in range(retries):
                     try:
                         response = requests.get(download_url, timeout=timeout_secs)
                         assert response, 'status_code=%d (%s)' % (response.status_code, requests.status_codes._codes.get(response.status_code, ['unofficial HTTP error'])[0])
                     except Exception as e:
-                        print(f'try #{j+1}/{retries}:', e)
+                        print(f'try {j+1}/{retries}:', e)
                 if not response:
                     bad_urls.append(download_url)
                     continue
-                cnt_dl += 1
                 content = response.content
                 if file_format == 'txt':
                     if strip_copyright:
@@ -99,9 +105,11 @@ with open('benyehuda_extra_files.txt', 'w') as fextra:
                 try:
                     have = False
                     for dup_period in all_periods:
+                        if dup_period == period:
+                            continue
                         dup_folder = os.path.join(base_folder, file_format, dup_period)
                         if os.path.exists(dup_folder):
-                            for dup_file in os.listdir(dup_folder):
+                            for dup_file in sorted(os.listdir(dup_folder)):
                                 if dup_file == file:
                                     dup_path = os.path.join(dup_folder, file)
                                     with open(dup_path, 'rb') as fdup:
@@ -113,6 +121,8 @@ with open('benyehuda_extra_files.txt', 'w') as fextra:
                                             os.remove(dup_path)
                                             print('Deleted identical file:', dup_path)
                     if not have:
+                        cnt_dl += 1
+                        total_dl += 1
                         with open(path, 'wb') as f:
                             f.write(content)
                 except KeyboardInterrupt:
@@ -120,21 +130,25 @@ with open('benyehuda_extra_files.txt', 'w') as fextra:
                         os.remove(path)
                     raise
             page += 1
+        all_periods_count += total_count
+        all_before_folder_files_count += before_folder_files_count
         folder_files = os.listdir(folder)
         print(f'Downloaded {cnt_dl}/{len(files)} giving {len(folder_files)} files for period={period}')
-        total_dl += cnt_dl
         total_files += len(folder_files)
-        extra_files = set(folder_files) - set(files)
+        extra_files = sorted(set(folder_files) - set(files))
         if extra_files:
             line = f'Note: found {len(extra_files)} extra files in {folder}: ' + ', '.join(extra_files)
             fextra.write(line + '\n')
             print(line)
-        for file in folder_files:
-            all_files[file].append(folder)
         print()
 
+all_files = defaultdict(list)
+for period in all_periods:
+    folder = os.path.join(base_folder, file_format, period)
+    for file in sorted(os.listdir(folder)):
+        all_files[file].append(folder)
+dupes = sorted((k, v) for k, v in all_files.items() if len(v) > 1)
 with open('benyehuda_dupe_names.txt', 'w') as fdup:
-    dupes = sorted((k, v) for k, v in all_files.items() if len(v) > 1)
     if dupes:
         print(f'Found {len(dupes)} duplicate names:')
         for file, folders in dupes:
@@ -145,12 +159,15 @@ with open('benyehuda_dupe_names.txt', 'w') as fdup:
                     contents.append(f.read())
             if all(content == contents[0] for content in contents[1:]):
                 line += ' (all identical)'
+            else:
+                line += ' (some differ)'
             fdup.write(line + '\n')
             print(line)
 
 if bad_urls:
     print(f'Found {len(bad_urls)} bad URLs:')
     with open('benyehuda_bad_urls.txt', 'a') as f:
+        f.write(f'\n{datetime.now()}\n')
         for url in sorted(bad_urls):
             f.write(url + '\n')
             print(url)
